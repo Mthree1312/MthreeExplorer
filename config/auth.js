6e1d5ad3385be0323aba2cd7d7d7f6ffe9409322
Rajan Pandya <rajankpandya@gmail.com>
2019-12-12 14:49:13 -0500

    google auth started

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+module.exports = 
+{
+    "google": {
+        "clientID": "593359421081-pqsqpjj6ram79fguk4i6ord47pj4crgo.apps.googleusercontent.com",
+        "clientSecret": "wcRt9k5Amzu_t0naxrnoP8Qr",
+        "callbackURL": "http://localhost:3000/auth/google/callback"
+    }
+}
